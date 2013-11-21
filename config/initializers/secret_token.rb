0c32aaf1c2a10574a7faeb4dc70618bdfba83bfc
Matjaz Demsar <mdemsar@gmail.com>
2013-11-21 17:34:49 +0100

    Vaje STE

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+B2ook::Application.config.secret_token = '5ab05c00c7f5e44226e52f43afb24677d9bac9d4a44edc20ce37fc06d7cd21b1fbd784c3f3d72b32a02c005467862f9a9d11a68c87c30faff2091aad31cec447'
